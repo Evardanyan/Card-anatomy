ee0ce98b5838770ce2945b01e7ded3cbc44a92ad
Edg4rV <vardedgar@mail.ru>
2020-12-17 00:56:42 +0400

    Stage 2/4 Luhn alorithm

--- banking.py
+++ banking.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 client_base = []
 balance = 0
@@ -16,29 +16,59 @@ def customer_balance():
 def random_with_n_digits(n):
     range_start = 10 ** (n - 1)
     range_end = (10 ** n) - 1
-    return randint(range_start, range_end)
+    return random.randint(range_start, range_end)
 
 
 def create_account():
-    card_number = "400000" + str(random_with_n_digits(10))
-    pin_number = str(random_with_n_digits(4))
+    sum_custom = 0
+    check_sum = 0
+    card_number = [4, 0, 0, 0, 0, 0]
+    card_number_str = 0
+    for i in range(10):
+        n = random.randint(0, 9)
+        card_number.append(n)
+    del card_number[-1]
+    card_number_temp = [x for x in card_number]
+    # card_number_temp[0] = 2 * card_number_temp[0]
+
+    for i in range(0, len(card_number_temp)):
+        if i % 2 == 0:
+            card_number_temp[i] = 2 * card_number_temp[i]
+
+    for i in range(len(card_number_temp)):
+        if card_number_temp[i] > 9:
+            card_number_temp[i] = card_number_temp[i] - 9
+
+    for i in range(len(card_number_temp)):
+        sum_custom = sum_custom + card_number_temp[i]
+    if sum_custom % 10 == 0:
+        check_sum = 0
+    else:
+        check_sum = 10 - sum_custom % 10
+
+    card_number.append(check_sum)
+
+    card_number_convert = [str(x) for x in card_number]
+    card_number_str = int("".join(card_number_convert))
+
+    pin_number = random_with_n_digits(4)
     global client_base
-    client_base.append(card_number)
+    client_base.append(card_number_str)
     client_base.append(pin_number)
     print("Your card has been created")
     print("Your card number:")
-    print(card_number)
+    print(card_number_str)
     print("Your card PIN:")
     print(pin_number)
-    print(client_base)
+    # print(client_base)
 
 
 def log_into_account():
     print("Enter your card number:")
-    customer_card_number = input()
+    customer_card_number = int(input())
     if customer_card_number in client_base:
         print("Enter your PIN:")
-        customer_pin_number = input()
+        customer_pin_number = int(input())
         if customer_pin_number in client_base:
             print("You have successfully logged in!")
             in_banking_system_message()
@@ -46,7 +76,7 @@ def log_into_account():
         else:
             print("Wrong card number or PIN!")
     elif customer_card_number not in client_base:
-                print("Wrong card number or PIN!")
+        print("Wrong card number or PIN!")
 
 
 def customer_out_from_system():
@@ -91,4 +121,3 @@ def out_from_banking_system_message():
 
 while check:
     customer_out_from_system()
-
